820ef8d388363881c64ff8b3c32551dd077ae18d
Pranay Harjai <pranayharjai7@gmail.com>
2026-04-23 17:44:48 +0200

    fixed build command smooth run

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,7 +4,7 @@ import GoogleProvider from "next-auth/providers/google"
 import { prisma } from "@/lib/prisma"
 
 export const authOptions: NextAuthOptions = {
-  adapter: PrismaAdapter(prisma),
+  adapter: PrismaAdapter(prisma as any),
   session: {
     strategy: "jwt",
   },
